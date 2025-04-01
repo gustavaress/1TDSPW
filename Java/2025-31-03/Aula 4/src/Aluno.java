4d2372e483f021d3757b7e545cbc6a87df189210
gustavaress <gustavares2005@gmail.com>
2025-04-01 17:57:32 -0300

    Estudo de Java 01/04

--- Java/2025-31-03/Aula 4/src/Aluno.java
+++ Java/2025-31-03/Aula 4/src/Aluno.java
@@ -7,32 +7,27 @@ public class Aluno {
     double notaCh;
     double notaGs;
 
-
-    // Calcular média CP
+    // Metodo calcular nota CP
     double calcularCp(){
         double mediaCp;
         mediaCp = (notaCp1 + notaCp2 + notaCp3)/3;
         return mediaCp;
     }
 
-    // Calcular media final
+    // Metodo calcular média final
     double calcularMedia(){
         double mediaFinal;
-        mediaFinal = notaCp*0.20 + notaCh*0.20 + notaGs*0.60;
+        mediaFinal = notaCp * 0.2 + notaCh * 0.2 + notaGs * 0.6;
         return mediaFinal;
-
-
     }
 
-    // Exibir infos
-    public void exibirNotas() {
-        System.out.println("Nome: " + nome);
-        System.out.println("Notas:");
+    public void exibirInfos(){
+        System.out.println("A nota do aluno " + nome + " serão:");
         System.out.println("Checkpoint: " + notaCp);
-        System.out.println("Challange: " + notaCh);
+        System.out.println("Challanger " + notaCh);
         System.out.println("Global Solution: " + notaGs);
 
         double notaFinal = calcularMedia();
-        System.out.println("Nota final: " + notaFinal);
+        System.out.println("Média final: " + notaFinal);
     }
 }
--- Java/2025-31-03/Aula 4/src/Main.java
+++ Java/2025-31-03/Aula 4/src/Main.java
@@ -2,42 +2,35 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner leitor = new Scanner(System.in);
 
-        // Cria um objeto de um aluno e atribui os valores de turma, nome e rm
         Aluno aluno1 = new Aluno();
 
+        System.out.println("Olá! Seja bem vindo ao calculador de média FIAP");
+        System.out.println(" ");
+
+        // Perguntar os dados do aluno
         System.out.println("Digite o nome do aluno: ");
         aluno1.nome = leitor.next() + leitor.nextLine();
 
-        // Atribuir os valores ao objeto notas
-        System.out.println("Digite a nota do 1 CheckPoint: ");
+        // Perguntar as notas do aluno;
+        System.out.println("Digite a nota do primeiro Checkpoint do aluno: ");
         aluno1.notaCp1 = leitor.nextDouble();
 
-        System.out.println("Digite a nota do 2 CheckPoint: ");
+        System.out.println("Digite a nota do segundo Checkpoint do aluno: ");
         aluno1.notaCp2 = leitor.nextDouble();
 
-        System.out.println("Digite a nota do 3 CheckPoint: ");
+        System.out.println("Digite a nota do terceiro Checkpoint do aluno: ");
         aluno1.notaCp3 = leitor.nextDouble();
 
-        System.out.println("Digite a nota do Challange: ");
+        System.out.println("Digite a nota de Challange do aluno: ");
         aluno1.notaCh = leitor.nextDouble();
 
-        System.out.println("Digite a nota do GlobalSolution: ");
+        System.out.println("Digite a nota da Global Solution do aluno: ");
         aluno1.notaGs = leitor.nextDouble();
 
-        // Executando metodo que calcula nota media do CP
         aluno1.notaCp = aluno1.calcularCp();
 
-        // Exibir infos
-        System.out.println("Nome: " + aluno1.nome);
-        System.out.println("Notas:");
-        System.out.println("Checkpoint: " + aluno1.notaCp);
-        System.out.println("Challange: " + aluno1.notaCh);
-        System.out.println("Global Solution: " + aluno1.notaGs);
-
-        aluno1.calcularMedia();
-        aluno1.exibirNotas();
+        aluno1.exibirInfos();
     }
 }
